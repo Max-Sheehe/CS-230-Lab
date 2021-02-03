ee51726294176d9f454d62aeb3ff8b4846b6bc64
Max-Sheehe <mms0079@mix.wvu.edu>
2021-02-02 21:22:25 -0500

    lab 2 - about page

---
+++ index.php
@@ -0,0 +1,52 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>hfejkdsafhjkfdsjlhdfsjkhf</p>
+    </div>  
+    <div class="row">
+
+        <div class="column"> 
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p style="color: red">spike</p>
+                    <p>Email: spike@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column"> 
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Quality Control</h2>
+                    <p class="c2">mugen n jin</p>
+                    <p>Email: sunflowers@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column"> 
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>Sales</h2>
+                    <p>asuka</p>
+                    <p>Email: asuka@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column"> 
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>Human Resources</h2>
+                    <p>iroh</p>
+                    <p>Email: iroh@cs230.com</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
